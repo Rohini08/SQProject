218787d1512aefffbf595aeffabd0ddff6f38a66
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2022-12-01 19:56:31 -0500

    feat: Add division operation, Calculator main logic and Calculator instance

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SQProject
+
+Simple Calculator example of handling different changes using a Version Control System with various members involved in the same project.
--- SimpleCalculator/src/com/tbd/sqt/Output.java
+++ SimpleCalculator/src/com/tbd/sqt/Output.java
@@ -1,9 +1,25 @@
 package com.tbd.sqt;
+import java.util.Scanner;  // Import the Scanner class
 
 public class Output {
 	
 	public static void main(String[] args) {
-		
+		Scanner sc= new Scanner(System.in); // Declare input from scanner that take System.in
+
+		System.out.print("What operation you wish? ('+', '-', '*', '/'): \n");
+		String operator = sc.next();
+		System.out.printf("Operator was: %s \n", operator);
+
+		System.out.print("Please enter 1st number: ");
+		double numberOne = sc.nextInt();
+
+		System.out.print("Please enter 2nd number: ");
+		double numberTwo = sc.nextInt();
+
+		Calculator calcObj = new Calculator(numberOne, numberTwo);
+		calcObj.calc(operator);
+		calcObj.getResult();
+
 	}
 
 }
